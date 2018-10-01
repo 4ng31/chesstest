0474199c3d3962260c55509c9ec8a3bb96147a27
4ng31 <7303724+4ng31@users.noreply.github.com>
2018-10-01 14:00:46 -0300

    random move

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import chess
 import random
+import numpy as np
+import math
+
 
 
 def bestMove(game):
     ugly_moves = game.legal_moves
-    return ugly_moves[1, len(ugly_moves)]
+    return random.choice(list(ugly_moves))
+
+if __name__ == '__main__':
+    game = chess.Board()
+    themove=bestMove(game)
+    print themove
