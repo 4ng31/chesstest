1af9fabebd50e246f67a689bfa7deef74bad8903
sinaxtis <maltamira@mendoza.gov.ar>
2018-10-02 13:53:56 -0300

    now you can play with the machine

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ import random
 
 def bestMove(game):
     ugly_moves = game.legal_moves
-    return random.choice(list(ugly_moves))
+    return list(ugly_moves)
 
 if __name__ == '__main__':
     game = chess.Board()
-    themove=bestMove(game)
-    print themove
+    playing = True
+    while playing:
+        themove = random.choice(bestMove(game))
+        print(themove)
+        game.push(themove)
+        print(game)
+        #print(bestMove(game)[Move.from_uci])
+        print("Posible moves: ")
+        for mov in bestMove(game):
+            print(mov)
+        move = input("Enter your move: ")
+        if move in bestMove(game):
+            game.push(move)
+        playing = not game.is_checkmate()
+        
+    
