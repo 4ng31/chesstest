395969c5e5288e999081252704098f4076e80d26
4ng31 <7303724+4ng31@users.noreply.github.com>
2018-10-02 14:24:04 -0300

    Some order

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+#!/usr/bin/python3
+
+import sys
 import chess
 import random
+
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 or a more recent version is required.")
+
 
 def bestMove(game):
     ugly_moves = game.legal_moves
     return list(ugly_moves)
 
-if __name__ == '__main__':
+def main():
+    """Beat The Turk"""
     game = chess.Board()
     playing = True
     while playing:
@@ -13,7 +21,6 @@ if __name__ == '__main__':
         print(themove)
         game.push(themove)
         print(game)
-        #print(bestMove(game)[Move.from_uci])
         print("Posible moves: ")
         for mov in bestMove(game):
             print(mov)
@@ -21,5 +28,6 @@ if __name__ == '__main__':
         if move in bestMove(game):
             game.push(move)
         playing = not game.is_checkmate()
-        
-    
+
+if __name__ == '__main__':
+    main()
