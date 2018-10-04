2191b162ce5edf8d8f6731c4572536fbf1784f60
4ng31 <7303724+4ng31@users.noreply.github.com>
2018-10-04 14:27:38 -0300

    Pretty print

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import sys
 import chess
 import random
 import numpy as np
+from os import system
+
 
 if sys.version_info[0] < 3:
     raise Exception("Python 3 or a more recent version is required.")
 
+def clear():
+        _ = system('clear')
 
 def listMoves(game):
     return list(game.legal_moves)
@@ -17,7 +21,6 @@ def calculateBestMove(game):
     for move in moves:
         _game = game
         _game.push(move)
-
 
 def evaluateBoard(board):
     fen = (board.fen().split(' '))[0]
@@ -36,20 +39,28 @@ def evaluateBoard(board):
         'k': -1000
     }
     fen = fen.replace("/","")
-    fen = ''.join(i for i in fen if  not i.isdigit())
+    fen = ''.join(i for i in fen if not i.isdigit())
     lst = np.asarray([piece[k] for k in list(fen)], dtype=np.int32)
     print(lst.sum())
 
-    #value = 0
-    # while i < len(fen[0]):
-    #     value = value + piece[i]
-    # for k, v in piece.items():
-    #     if f in fen[0]:
-    #         if f == piece[k]:
-    #             value = value + piece[k]
-    #             print("Code : {0}, Value : {1}".format(k, v))
-    # print(value)
+def print_board(board):
+    clear()
+    PIECE_SYMBOLS = {'P': '♟', 'B': '♝', 'N': '♞',
+                     'R': '♜', 'Q': '♛', 'K': '♚',
+                     'p': '♙', 'b': '♗', 'n': '♘',
+                     'r': '♖', 'q': '♕', 'k': '♔'}
+    fen = (board.fen().split(' '))[0]
+    b = str(board)
+    for k, v in PIECE_SYMBOLS.items():
+        b = b.replace("{}".format(k), "{}".format(v))
+    b = b.split('\n')
+    col = [8,7,6,5,4,3,2,1]
+    row = "A B C D E F G H"
 
+    for i in range(0,8,1):
+        print("%s %s" %(col[i],b[i]))
+    print("  %s"%row)
+    #print(fen)
 
 def main():
     """Beat The Turk"""
@@ -61,22 +72,22 @@ def main():
         board.push(themove)
         print_board(board)
         print("Posible moves: ")
+
+        moves = []
         for mov in listMoves(board):
-            print(mov)
+            
+            moves.append(str(mov))
+
+        for i in range(0, len(moves), 4):
+            print(", ".join(moves[i:i+4]))
+
         move = input("Enter your move: ")
+        clear()
+
         if move in listMoves(board):
             board.push(move)
         evaluateBoard(board)
         playing = not board.is_checkmate()
 
-def print_board(board):
-        PIECE_SYMBOLS = {'P': '♟', 'B': '♝', 'N': '♞', 'R': '♜', 'Q': '♛', 'K': '♚', 'p': '♙', 'b': '♗', 'n': '♘', 'r': '♖', 'q': '♕', 'k': '♔'}
-        fen = (board.fen().split(' '))[0]
-        b = str(board)
-        for k, v in PIECE_SYMBOLS.items():
-            #fen = fen.replace("{}".format(k),"{}".format(v))
-            b = b.replace("{}".format(k),"{}".format(v))
-        print(b)
-        print(fen)
 if __name__ == '__main__':
     main()
